147c694d08e76cda3187cd4702832dc5c50e93d1
lalilaloe <lalilaloe@hotmail.com>
2023-05-15 18:29:17 +0200

    fix(build): Workaround importing from libs

---
+++ src/services/roles.ts
@@ -0,0 +1,90 @@
+// TODO: fix build Error: 'isAuthor' is not exported by ../libs/services/roles.js, imported by src/events/index.svelte on build
+import Permissions from "@models/Permissions/Permissions";
+
+export function getPermissions(roles, groups) {
+  roles = roles.split(",");
+  roles = roles.map((role) => role.toLowerCase().trim());
+  const permissions = [];
+
+  for (const role of roles) {
+    if (role === "member") {
+    }
+    if (role === "moderator") {
+    }
+
+    //TODO, return persmission ENUM based on string
+  }
+
+  if (groups?.length) {
+    for (const group of groups) {
+      if (group.name.includes(".com")) {
+        permissions.push(Permissions.EVENTS.ALL);
+      }
+    }
+  }
+
+  return new Set(roles); //permissions
+}
+
+export function isAdmin(entitlements) {
+  if (entitlements.has("admin")) return true;
+  return false;
+}
+
+export function isAuthor(content, sessionStorage, session) {
+  let author;
+  if (content?.organiser) {
+    if (content.organiser.organiser === "Group") {
+      author = sessionStorage.groups.find(
+        (g) => g._id === content.organiser?.group?._id
+      );
+    }
+    if (content.organiser.organiser.match(/IFES|Landelijk/)) {
+      author = sessionStorage.groups.find((g) => g.name === "Bestuur");
+    }
+  } else {
+    author = (content.author === sessionStorage.user._id || content.author?._id === sessionStorage?.member?._id && !sessionStorage?.member?._id === null) || isAdmin(session.getEntitlements);
+  }
+  return !!author;
+}
+
+export function hasRole(
+  roles: Array<string>,
+  entitlements: Set<string>,
+  checkIfUserHasRoles = false,
+  masterRole: string = ""
+) {
+  if (checkIfUserHasRoles) return entitlements.size > 1;
+  if (entitlements.has(masterRole + ".all") || isAdmin(entitlements)) {
+    return true;
+  } else {
+    return roles.filter((role) => entitlements.has(role))?.length;
+  }
+}
+
+// TODO: better way of coupling roles to pages, maybe use metadata https://routify.dev/docs/metadata
+export function hasRolesForPage(currentPage, adminRoutes, entitlements) {
+  if (isAdmin(entitlements)) return true;
+  if (currentPage.match(/\/$/))
+    currentPage = currentPage.replace(/\/$/, "/index");
+  for (const route of adminRoutes) {
+    if (route.pages.some((url) => url === currentPage)) {
+      for (const role of entitlements) {
+        if (role.match(route.role)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+export function hasRolesForModel(role, items, entitlements) {
+  if (isAdmin(entitlements)) return true;
+  for (const item of items) {
+    if (item.role.some((r) => hasRole([r], entitlements, false, role))) {
+      return true;
+    }
+  }
+  return false;
+}
