89732faad3841c5cbf9645d1f53cea075960d93a
lalilaloe <lalilaloe@hotmail.com>
2023-05-24 14:55:27 +0200

    fix(deploy): Remove scripts and use /graphql for prod

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,1 +1,1 @@
-export const GRAPHQL_URI = process.env.NODE_ENV === "production" ? "https://dev.ichthuszwolle.nl/graphql": `http://localhost:${SERVER_PORT}/graphql`;
+export const GRAPHQL_URI = process.env.NODE_ENV === "production" ? "/graphql": `http://localhost:${SERVER_PORT}/graphql`;
