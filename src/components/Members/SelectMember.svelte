3c9f47a9106d06a941967adf7ab1dbe83b609f93
lalilaloe <lalilaloe@hotmail.com>
2023-05-29 23:05:30 +0200

    feat(events): Select members modal & Document upload begin

--- src/components/Members/SelectMember.svelte
+++ src/components/Members/SelectMember.svelte
@@ -1,17 +1,19 @@
 <script>
-    import { onMount} from "svelte"
-    import "@static/assets/js/plugins/choices.min.js"
+  import { onMount } from "svelte";
+  import "@static/assets/js/plugins/choices.min.js";
 
-    import { memberManySelect } from "@models/Member/member.gql";
+  import { memberManySelect } from "@models/Member/member.gql";
   import { stripResult } from "@utils/gql";
   import { query } from "svelte-apollo";
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
 
-    export let members = [];
-    export let labelText = "Selecteer leden"
-    export let selectedIds = []
-    export let selectedMembers = []
+    import { onDestroy } from 'svelte';
+
+  export let members = [];
+  export let labelText = "Selecteer leden";
+  export let selectedIds = [];
+  export let selectedMembers = [];
   const membersQuery = query(memberManySelect, { variables: { filter: {} } });
 
   $: if ($membersQuery.data) {
@@ -20,49 +22,60 @@
     dispatch("error", $membersQuery.error);
   }
 
-let select;
-let selectChoice;
-let mounted = false;
+  let select;
+  let selectChoice;
+  let mounted = false;
 
-onMount(() => {
-  mounted = true
-});    
+  onMount(() => {
+    mounted = true;
+  });
 
-$: if(mounted && $membersQuery.data) {
-    const values = members.map((member) => {
-        return {
-  value: member._id,
-  label: member.fullName,
-  username: member.user?.username || ""
-}});
-    selectChoice = new Choices(select, {
-        silent:true,
-        choices: values,
-        removeItemButton: true,
-        searchFields: ['label', 'value', 'username'],
-        loadingText: 'Loading...',
-    noResultsText: 'Geen leden gevonden',
-    noChoicesText: 'Geen leden om uit te kiezen',
-    itemSelectText: 'Klik om te selecteren',
-    })
+  const valueMapper = (member) => 
+    {
+      return {
+        value: member._id,
+        label: member.fullName,
+        username: member.user?.username || "",
+      };
+    }
   
-}
 
-function updateSelected(){
-    if(mounted && members.length) {
-    selectedIds = selectChoice.getValue(true)
-    selectedMembers = members.filter(m => selectedIds.includes(m._id));
-}
-}
+  $: if (mounted && $membersQuery.data) {
+    const values = members.map(valueMapper);
+    selectChoice = new Choices(select, {
+      choices: values,
+      removeItemButton: true,
+      searchFields: ["label", "value", "username"],
+      loadingText: "Loading...",
+      noResultsText: "Geen leden gevonden",
+      noChoicesText: "Geen leden om uit te kiezen",
+      itemSelectText: "Klik om te selecteren",
+    });
+    selectChoice.setValue(selectedMembers.map(valueMapper))
+    console.log(selectChoice.getValue(true))
+    mounted = false
+  }
 
-$: if(selectChoice && !selectedMembers.length) {
-    console.log("reset")
-    selectChoice.destroy()
-    selectChoice.init()
-}
+  function updateSelected() {
+    if (members.length) {
+      selectedIds = selectChoice.getValue(true);
+      console.log(selectedIds)
+      selectedMembers = members.filter((m) => selectedIds.includes(m._id));
+    }
+  }
+
+  onDestroy(() => {
+    selectChoice.destroy();
+    dispatch("destroyed")
+  });
+
+  $: if (selectChoice && !selectedMembers.length) {
+    selectChoice.destroy();
+    selectChoice.init();
+  }
 </script>
 
 <div class="tw-w-4/5 z-index-3">
-    <label class="tw-mt-6 tw-mb-2 tw-ml-1 tw-font-bold tw-text-xs tw-text-slate-700 dark:tw-text-white/80" for="Skills">{labelText}</label>
-    <select multiple bind:this={select} on:change={updateSelected}/>
+  <label class="tw-mt-6 tw-mb-2 tw-ml-1 tw-font-bold tw-text-xs tw-text-slate-700 dark:tw-text-white/80" for="Skills">{labelText}</label>
+  <select multiple bind:this={select} on:change={updateSelected} />
 </div>
