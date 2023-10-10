84e85fa2044bba162ce7ef8e1b8048b1c221132f
lalilaloe <lalilaloe@hotmail.com>
2023-10-10 13:08:21 +0200

    feat(transistion): Event delayed by index animation

--- src/events/index.svelte
+++ src/events/index.svelte
@@ -69,76 +69,6 @@
 
   const close = () => editEvent.set({});
 
-  const expand = (node, { id }) => {
-    function get_state(node) {
-      const rect = node.getBoundingClientRect();
-      const style = getComputedStyle(node);
-      const transform_origin = style.transformOrigin.split(" ").map(parseFloat);
-      const cx = rect.left + transform_origin[0];
-      const cy = rect.top + transform_origin[1];
-
-      const brad = [
-        parse_brad(style.borderTopLeftRadius, rect),
-        parse_brad(style.borderTopRightRadius, rect),
-        parse_brad(style.borderBottomRightRadius, rect),
-        parse_brad(style.borderBottomLeftRadius, rect),
-      ];
-
-      const border = [
-        parseFloat(style.borderTopWidth),
-        parseFloat(style.borderRightWidth),
-        parseFloat(style.borderBottomWidth),
-        parseFloat(style.borderLeftWidth),
-      ];
-
-      return { cx, cy, rect, brad, border };
-    }
-
-    const source = get_state(document.querySelector(`[data-id="${id}"]`));
-    const target = get_state(node);
-
-    return {
-      duration: 400,
-      easing: eases.quintInOut,
-      css: (t, u) => {
-        const tx = u * (source.cx - target.cx);
-        const ty = u * (source.cy - target.cy);
-        const translate = `translate(${tx}px,${ty}px)`;
-
-        const sx = t + (u * source.rect.width) / target.rect.width;
-        const sy = t + (u * source.rect.height) / target.rect.height;
-        const scale = `scale(${sx},${sy})`;
-
-        const brad_x_source = source.brad.map((a) => a[0]);
-        const brad_x_target = target.brad.map((a) => a[0]);
-        const brad_x = brad_x_source.map((source, i) => {
-          const target = brad_x_target[i];
-          return `${(target + u * (source - target)) / sx}px`;
-        });
-
-        const brad_y_source = source.brad.map((a) => a[1]);
-        const brad_y_target = target.brad.map((a) => a[1]);
-        const brad_y = brad_x_source.map((source, i) => {
-          const target = brad_y_target[i];
-          return `${(target + u * (source - target)) / sy}px`;
-        });
-
-        const brad = `${brad_x.join(" ")} / ${brad_y.join(" ")}`;
-
-        const target_border = target.border;
-        const border = source.border
-          .map((source, i) => {
-            const target = target_border[i];
-            const s = i % 2 ? sy : sx;
-            return `${(target + u * (source - target)) / s}px`;
-          })
-          .join(" ");
-
-        return `transform: ${translate} ${scale}; border-radius: ${brad}; border-width: ${border}`;
-      },
-    };
-  };
-
   let editEvent = writable({});
   let editEventModalElement;
   let editEventModal;
@@ -151,15 +81,15 @@
     if (!events?.length) {
       events = stripResult($eventsQuery.data);
       let i = 0;
-      while (i < events.length) {
-        let event = events[i];
+      // while (i < events.length) {
+      //   let event = events[i];
 
-        if ((event.to != null && new Date(event.to) < new Date()) || (event.to == null && new Date(event.from) < new Date())) {
-          events.splice(i, 1);
-        } else {
-          i++;
-        }
-      }
+      //   if ((event.to != null && new Date(event.to) < new Date()) || (event.to == null && new Date(event.from) < new Date())) {
+      //     events.splice(i, 1);
+      //   } else {
+      //     i++;
+      //   }
+      // }
 
       events.sort(function (a, b) {
         return new Date(a.from) - new Date(b.from);
@@ -184,17 +114,17 @@
     {#if $eventsQuery.loading}
       <Loading />
     {:else if $eventsQuery.data}
-      {#each events as event}
+      {#each events as event, index}
         <div class="py-2">
           <!-- {#if event.type?.match(/kring/) || event.tags?.match(/Incasso|incasso/)} -->
           {#if $editEvent?._id == event._id}
             <div class="py-2">
-              <!-- <div  transition:expand={{}}> -->
               <EditEvent event={$editEvent} on:save={() => update(event)} on:close={close} />
             </div>
           {:else}
             <Event
               {event}
+              i={index}
               author={isAuthor(event, $currentSession, session)}
               on:edit={({ detail }) => {
                 editEvent.set({ ...detail });
