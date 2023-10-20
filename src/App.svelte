91e4bbce5991050ffd8fb8d2bd94432545a83f02
lalilaloe <lalilaloe@hotmail.com>
2023-10-20 19:10:15 +0200

    chore(loading): Load scripts in app prevent long white screen

--- src/App.svelte
+++ src/App.svelte
@@ -23,6 +23,16 @@
   };
 </script>
 
+<svelte:head>
+  <!-- Pusher -->
+	<script src="https://js.pusher.com/7.2/pusher.min.js"></script>
+	<script async src="//cdn.headwayapp.co/widget.js"></script>
+  
+  <!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+</svelte:head>
+
 <script>
   updateGraphqlClient();
 </script>
